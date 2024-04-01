d1df7d545c4ab653ab5b9c332a35b10d38faa2b8
kamzhanyue <kamzhanyue@gmail.com>
2024-04-02 03:30:38 +1100

    Add Intro Animation

--- Assets/Systems/Fishing/OldGod.cs
+++ Assets/Systems/Fishing/OldGod.cs
@@ -13,7 +13,7 @@ using UnityEngine.InputSystem;
 public class OldGod : MonoBehaviour
 {
     private const int SCALE_STEP = 5;
-    [BoxGroup("Scriptable Objects"), SerializeField] private FloatVariable scale;
+    [BoxGroup("Scriptable Objects"), SerializeField] private GameSettings gameSettings;
     [BoxGroup("Components"), SerializeField] private Casting casting;
     [BoxGroup("Components"), SerializeField] private ShootTowards shootTowards;
     private State _state;
@@ -31,6 +31,18 @@ public class OldGod : MonoBehaviour
     private void Start()
     {
         _main = Camera.main;
+        if (gameSettings.showIntroAnimation)
+        {
+            DOTween.To(SetCameraSize, 100f, 5f, 1.2f).SetEase(Ease.InOutQuint).OnComplete(Init);
+        }
+        else
+        {
+            Init();
+        }
+    }
+
+    private void Init()
+    {
         _playerControls = new PlayerControls();
         _playerControls.Game.Shoot.performed += Shoot;
         _playerControls.Enable();
--- Assets/Systems/GameSettings.cs
+++ Assets/Systems/GameSettings.cs
@@ -3,5 +3,7 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "ScriptableObject/GameSettings")]
 public class GameSettings : ScriptableObject
 {
+    public bool showIntroCinematic = true;
+    public bool showIntroAnimation = true;
     public int initialRings = 10;
 }
