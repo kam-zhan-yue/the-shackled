ecfbc0b3a1b1aeb01aa1323aa984aa99047f34a9
kamzhanyue <kamzhanyue@gmail.com>
2024-04-01 14:54:00 +1100

    Fail changes

--- Assets/Systems/Universe/CelestialBody.cs
+++ Assets/Systems/Universe/CelestialBody.cs
@@ -8,10 +8,13 @@ using UnityEngine;
 public class CelestialBody : MonoBehaviour
 {
     public bool clockwiseOrbit = false;
-    public float orbitalSpeed = 5f;
+    [Range(0f, 360f)] public float startAngle = 0f;
+    public float orbitalPeriod = 1f;
+    [Range(1f, 5f)]
+    public float orbitalRadius = 1f;
     public BodyType bodyType;
     [HideIf("bodyType", BodyType.Center)] 
-    public CelestialBody parent;
+    public Transform parent;
     private Rigidbody2D _rigidbody;
 
     private float _angle;
@@ -23,27 +26,51 @@ public class CelestialBody : MonoBehaviour
         _rigidbody = GetComponent<Rigidbody2D>();
     }
 
+    private void Start()
+    {
+        InitialiseDistance();
+    }
+
+    private void InitialiseDistance()
+    {
+        transform.position = GetStartPosition();
+    }
+
+    // Function to convert angle (in degrees) to rotation vector (x, y)
+    public Vector2 ConvertAngleToRotation(float angleDegrees)
+    {
+        // Convert angle from degrees to radians
+        float angleRadians = angleDegrees * Mathf.Deg2Rad;
+
+        // Calculate x and y components of the rotation vector
+        float x = Mathf.Cos(angleRadians);
+        float y = Mathf.Sin(angleRadians);
+
+        return new Vector2(x, y);
+    }
+    
+    private Vector2 GetStartPosition()
+    {
+        Vector2 rotation = ConvertAngleToRotation(startAngle);
+        Vector3 parentPos = parent.transform.position;
+        Vector2 direction = transform.position - parentPos;
+        return parentPos + (Vector3)rotation.normalized * orbitalRadius;
+    }
+
     private void FixedUpdate()
     {
         if (bodyType != BodyType.Center)
         {
             SetVelocity();
             _rigidbody.MovePosition(_rigidbody.position + Velocity * Time.fixedDeltaTime);
-            Debug.Log($"Distance = {GetRadius()}");
         }
     }
 
     private void SetVelocity()
     {
-        Vector2 parentVelocity = parent.Velocity;
+        // Vector2 parentVelocity = parent.Velocity;
         Vector2 circularVelocity = GetCircularVelocity();
-        Velocity = parentVelocity + circularVelocity;
-    }
-
-    private float GetRadius()
-    {
-        Vector2 difference = parent.transform.position - transform.position;
-        return difference.magnitude;
+        Velocity =  circularVelocity;
     }
 
     private Vector2 GetCircularVelocity()
@@ -54,8 +81,14 @@ public class CelestialBody : MonoBehaviour
         Vector2 direction = clockwiseOrbit
             ? new Vector2(-difference.y, difference.x)
             : new Vector2(difference.y, -difference.x);
-        Vector2 circularVelocity = direction.normalized * orbitalSpeed;
+        float circularSpeed = 2 * Mathf.PI * orbitalRadius / orbitalPeriod;
+        Vector2 circularVelocity = direction.normalized * circularSpeed;
         return circularVelocity;
+    }
+
+    private void OnValidate()
+    {
+        transform.position = GetStartPosition();
     }
 
     private void OnDrawGizmos()
@@ -66,6 +99,8 @@ public class CelestialBody : MonoBehaviour
             Vector3 position = transform.position;
             Vector2 endPoint = (Vector2)position + circularVelocity;
             Gizmos.DrawLine(position, endPoint);
+            
+            Gizmos.DrawSphere(GetStartPosition(), 0.1f);
         }
     }
 }
